d879885b033874877c12cc8993e0fd03a3bb0a59
Hakan Akdoğan <akdoganhakann@gmail.com>
2022-06-03 21:41:14 +0300

    Housekeeping in the Startup class

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
+using System.Collections.Generic;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+
+            });
+            services.AddCors(opt => opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+            }));
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+            return services; 
+        }
+    }
+}
